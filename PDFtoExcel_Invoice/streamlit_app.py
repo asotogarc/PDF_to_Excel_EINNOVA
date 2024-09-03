dffcf501b6626d2db718ea993963ca9ca77695c5
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-03 12:51:24 +0200

    Update streamlit_app.py

--- PDFtoExcel_Invoice/streamlit_app.py
+++ PDFtoExcel_Invoice/streamlit_app.py
@@ -125,7 +125,7 @@ st.markdown("""
         box-shadow: 0 4px 8px rgba(0,0,0,0.1);
     }
     h1, h2, h3 {
-        color: #2C3E50;
+        color: #3A5199;
         font-family: 'Helvetica Neue', sans-serif;
         text-align: center;
     }
