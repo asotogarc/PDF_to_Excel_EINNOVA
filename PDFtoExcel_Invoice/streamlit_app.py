1f36f9dea959b2245ae8ae021839fa12215efc5c
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-03 16:47:16 +0200

    Update streamlit_app.py

--- PDFtoExcel_Invoice/streamlit_app.py
+++ PDFtoExcel_Invoice/streamlit_app.py
@@ -98,7 +98,7 @@ st.set_page_config(page_title="Analizador Inteligente de Facturas", layout="wide
 st.markdown("""
     <style>
     .main {
-        background-color: #ffffff;
+        background-color: #90AFC5;
         padding: 2rem;
         border-radius: 10px;
     }
